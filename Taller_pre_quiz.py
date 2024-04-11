f7e1f30f8dcab323df7361335d5b4aa3fc8484bd
sarapalaciod <sarapalaciod@udea.edu.co>
2024-04-11 09:04:40 -0500

    Punto 6: Se creo una funcion llamada cargar_archivo en la cual si detecta un .mat o un .csv  con un endswirh; lee los archivos, pero si estos no son ninguno de los dos no lo va poder leer

--- Taller_pre_quiz.py
+++ Taller_pre_quiz.py
@@ -1,5 +1,6 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
+import scipy.io as sp  
 
 matriz_aleatoria= np.random.randint(11, size=(20,30,20,100))
 
@@ -21,3 +22,13 @@ def matriz_dataframe(m):
     return df 
 data_resultado= matriz_dataframe(matriz_2d)
 print(f'Data Frame: {data_resultado}')
+
+def cargar_archivo(a):
+    if a.endswith('.mat'):
+        datos= sp.loadmat(a)
+        return datos
+    elif a.endswith('.csv'):
+        datos= pd.read_csv(a, encoding='utf-8')
+        return datos 
+    else:
+        print ('Archivo no compatible')
