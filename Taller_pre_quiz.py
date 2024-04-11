a3d884180911bf1fa244da53528b0b25c25cfda8
sarapalaciod <sarapalaciod@udea.edu.co>
2024-04-11 08:47:15 -0500

    punto 2: Se realizo una copia de la matriz principal y a esta una de las dimensiones se volvio en 0, por ende obtenemos como si fuera de 3 dimensiones

--- Taller_pre_quiz.py
+++ Taller_pre_quiz.py
@@ -1,6 +1,8 @@
 import numpy as np 
+
 matriz_aleatoria= np.random.randint(11, size=(20,30,20,100))
 
-print(f'Matriz: ', matriz_aleatoria)
-print(f'Dimension de la matriz: ', matriz_aleatoria.ndim) 
-print(f'Tamaño de la matriz: ', matriz_aleatoria.size)
+nueva_matriz= matriz_aleatoria.copy()
+nueva_matriz = nueva_matriz[:, :, :, 0]
+
+print(f'Nueva matriz: {nueva_matriz}')
