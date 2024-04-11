dc7265e26078a8ae7fe909a20be5e2407836214a
sarapalaciod <sarapalaciod@udea.edu.co>
2024-04-11 08:53:21 -0500

    Cambio a la matriz original

--- Taller_pre_quiz.py
+++ Taller_pre_quiz.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-matriz_aleatoria= np.random.randint(11, size=(20,30,20,1000))
+matriz_aleatoria= np.random.randint(11, size=(20,30,20,100))
 
 nueva_matriz= matriz_aleatoria.copy()
 nueva_matriz = nueva_matriz[:, :, :, 0]
