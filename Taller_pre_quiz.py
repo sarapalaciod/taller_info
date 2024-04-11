27eb1ea98aa7b770bd59fa5401b688c676aba102
sarapalaciod <sarapalaciod@udea.edu.co>
2024-04-11 08:51:48 -0500

    punto 3: Se mostraton los atributos de la matriz nueva que se creo de 3 dimensiones

--- Taller_pre_quiz.py
+++ Taller_pre_quiz.py
@@ -1,8 +1,12 @@
 import numpy as np 
 
-matriz_aleatoria= np.random.randint(11, size=(20,30,20,100))
+matriz_aleatoria= np.random.randint(11, size=(20,30,20,1000))
 
 nueva_matriz= matriz_aleatoria.copy()
 nueva_matriz = nueva_matriz[:, :, :, 0]
 
 print(f'Nueva matriz: {nueva_matriz}')
+print(f'dimencion:',nueva_matriz.ndim)
+print(f'tamaño:',nueva_matriz.shape)
+print(f'numero de elementos:',nueva_matriz.size)
+print(f'tipo de datos:',nueva_matriz.dtype)
